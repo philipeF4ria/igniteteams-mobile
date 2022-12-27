af77438c7282f1d690f74b1ca3778452a02504ee
Philipe Faria <philipefaria61@gmail.com>
2022-12-27 20:14:36 -0300

    style: Create ListPlayers component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,6 +6,9 @@ import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
@@ -46,6 +49,28 @@ function Players() {
           {players.length}
         </NumberOfPlayers>
       </HeaderList>
+      <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard 
+            name={item}
+            onRemove={() => {}}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Não há pessoas neste time'/>
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+      <Button 
+        title='Remover turma'
+        type='SECONDARY'
+      />
     </Container>
   );
 }
